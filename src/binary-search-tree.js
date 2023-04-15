28cddb17aacabb9d4003bc3d7df4abdd3c9bae79
Anna Levkovich <7604442@gmail.com>
2023-04-15 10:15:40 +0300

    feat: to do tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,46 +6,118 @@ const { Node } = require("../extensions/list-tree.js");
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
+
 class BinarySearchTree {
   constructor() {
-    this.tree;
+    this.tree = null;
   }
   root() {
     return this.tree;
   }
 
   add(data) {
+    if (typeof data !== "number") {
+      return this.tree;
+    }
+    const node = new Node(data); // новая node (5 ждет куда)
+
     if (!this.tree) {
-      this.tree = new Node(data);
-      return;
+      this.tree = node;
+      return this.tree;
     }
-    let currentNode = this.tree;
-    while (currentNode.left && currentNode.right) {
-      if(currentNode.left){
-        
+    let currentNode = this.tree; //текущая node (10 верхушка)
+    let stop = false;
+    while (!stop) {
+      if (currentNode.data < node.data) {
+        if (currentNode.right === null) {
+          currentNode.right = node;
+          stop = true;
+        } else {
+          currentNode = currentNode.right;
+        }
+      } else {
+        if (currentNode.left === null) {
+          currentNode.left = node; //записали
+          stop = true; // остановили цикл
+        } else {
+          currentNode = currentNode.left;
+        }
       }
-      currentNode = currentNode.left;
-    }
-    if (currentNode.left && !currentNode.right) {
-      currentNode.right = new Node(data);
-    } else {
-      currentNode.left = new Node(data);
     }
   }
-
+  //data это аргумент функции
   has(data) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    //5
+    const foundNode = this.find(data);
+    if (foundNode) {
+      return true;
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  find(data) {
+    if (typeof data !== "number") {
+      return null;
+    }
+    let stop = false;
+    let currentNode = this.tree;
+    let foundNode;
+
+    while (!stop) {
+      if (!currentNode) {
+        stop = true;
+        foundNode = null;
+        return foundNode;
+      }
+      if (currentNode.data === data) {
+        stop = true;
+        foundNode = currentNode;
+      } else {
+        if (currentNode.data < data) {
+          // 7 < 5
+          currentNode = currentNode.right;
+        } else {
+          currentNode = currentNode.left;
+        }
+      }
+    }
+    return foundNode;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  remove(data) {
+    if (typeof data !== "number") {
+      return null;
+    }
+    let stop = false;
+    let currentNode = this.tree;
+    let foundNode;
+    let parentNode;
+    if (currentNode.data === data) {
+      this.tree = null;
+    }
+    while (!stop) {
+      if (currentNode.data === data) {
+        stop = true;
+        if (parentNode.left === currentNode) {
+          parentNode.left = null;
+        } else {
+          parentNode.right = null;
+        }
+      } else {
+        if (currentNode.data === null) {
+          stop = true;
+        }
+        if (currentNode.data < data) {
+          // 7 < 5
+          parentNode = currentNode;
+          currentNode = currentNode.right;
+        } else {
+          parentNode = currentNode;
+          currentNode = currentNode.left;
+        }
+      }
+    }
+    return foundNode;
   }
 
   min() {
@@ -58,7 +130,29 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 }
+// const tree = new BinarySearchTree();
 
+// tree.add(9);
+// tree.add(14);
+// tree.add(2);
+// tree.add(6);
+// tree.add(128);
+// tree.add(8);
+// tree.add(31);
+// tree.add(54);
+// tree.add(1);
+// tree.remove(14);
+// tree.remove(8);
+// tree.remove(9);
+// console.log(tree.has(14), false);
+// console.log(tree.has(8), false);
+// console.log(tree.has(9), false);
+// console.log(tree.has(2), true);
+// console.log(tree.has(6), true);
+// console.log(tree.has(128), true);
+// console.log(tree.has(31), true);
+// console.log(tree.has(54), true);
+// console.log(tree.has(1), true);
 module.exports = {
   BinarySearchTree,
 };
