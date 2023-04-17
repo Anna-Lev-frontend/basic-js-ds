05c157995922cffae57f3b2952c3e197fb86e9de
Anna Levkovich <7604442@gmail.com>
2023-04-17 21:54:37 +0300

    feat: to do remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function removeKFromList(l, k) {
+  if (l === null) {
+    return 1;
+  }
 
+  
+let element = l;
+let nextElement = element.next
+
+  while (nextElement) {
+    if (nextElement.value === k) {
+      element.next = nextElement.next;
+    } else {
+      element = nextElement.next;
+    }
+  }
+  return l;
+}
 module.exports = {
   removeKFromList
 };
